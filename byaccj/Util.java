2b0aaa1b128b8b7aa124c3ddf1b3ff192725dc24
ss4555 <ss4555@columbia.edu>
2014-04-26 16:21:45 -0400

    Util.java add writeCardjava

--- byaccj/Util.java
+++ byaccj/Util.java
@@ -27,4 +27,25 @@ public class Util {
 			e.printStackTrace();
 		}
 	}
+
+    public static writeCardsJava(String card_name, String method_content){
+         File outputFile = new File(card_name+".java");
+         try{
+            if(outputFile.exists()) {
+                outputFile.delete();
+            }
+            outputFile.createNewFile();          
+            BufferedWriter out = new BufferedWriter(new FileWriter(outputFile));
+            out.write("public class "+card_name+" implements Card\n");
+            out.write("public void method(Player dealer){\n");
+            out.write(method_content);
+            out.write("\n}\n");
+            out.flush();
+            out.close();
+         }catch(IOException ex){
+             ex.printStackTrace();
+         }
+
+
+
 }
